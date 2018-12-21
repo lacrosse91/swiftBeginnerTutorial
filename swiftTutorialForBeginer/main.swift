39d8e89d962b3e986ad06f67047398d57ab0c979
川内翔一朗 <>
2018-12-21 14:52:07 +0900

    first

---
+++ swiftTutorialForBeginer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftTutorialForBeginer
+//
+//  Created by 川内翔一朗 on 2018/12/21.
+//  Copyright © 2018 川内翔一朗. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
