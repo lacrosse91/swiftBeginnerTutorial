0da7188adeeecb40473ad4242fa457c854e3286f
川内翔一朗 <>
2018-12-22 20:34:18 +0900

    create operators

---
+++ swiftTutorialForBeginer/BasicOperators/Operators.playground/Contents.swift
@@ -0,0 +1,45 @@
+///////Assignment Operator ////
+
+let b = 10
+var a = 5
+a = b
+// a is now equal to 10
+
+let (x, y) = (1, 2)
+// x is equal to 1, and y is equal to 2
+
+//if x = y {
+//    // This is not valid, because x = y does not return a value.
+//}
+
+if x == y {
+    // This is OK
+}
+////////////////////////////////
+
+
+////////Arithmetic Operators/////////
+1 + 2       // equals 3
+5 - 3       // equals 2
+2 * 3       // equals 6
+10.0 / 2.5  // equals 4.0
+"hello, " + "world"  // equals "hello, world"
+
+//Remainder Operator
+9 % 4    // equals 1
+-9 % 4   // equals -1
+
+//The sign of b is ignored for negative values of b. This means that a % b and a % -b always give the same answer.
+
+
+//Unary Minus Operator
+let three = 3
+let minusThree = -three       // minusThree equals -3
+let plusThree = -minusThree   // plusThree equals 3, or "minus minus three"
+//Unary Plus Operator
+let minusSix = -6
+let alsoMinusSix = +minusSix  // alsoMinusSix equals -6
+/////////////////////////////////////
+
+
+
