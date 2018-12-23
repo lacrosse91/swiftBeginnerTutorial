35632c8f1727482d3c8500c88e0f266696ffc84b
川内翔一朗 <>
2018-12-24 00:30:53 +0900

    string index

--- swiftTutorialForBeginer/StringAndCharacters/AccessingAndModifyingAString.playground/Contents.swift
+++ swiftTutorialForBeginer/StringAndCharacters/AccessingAndModifyingAString.playground/Contents.swift
@@ -11,4 +11,27 @@ let index = greeting.index(greeting.startIndex, offsetBy: 7)
 greeting[index]
 // a
 
+//greeting[greeting.endIndex] // Error
+//greeting.index(after: greeting.endIndex) // Error
+
+for index in greeting.indices {
+//    print(index)
+    print("\(greeting[index]) ", terminator: "")
+}
+
+var welcome = "hello"
+welcome.insert("!", at: welcome.endIndex)
+// welcome now equals "hello!"
+
+welcome.insert(contentsOf: " there", at: welcome.index(before: welcome.endIndex))
+
+welcome.remove(at: welcome.index(before: welcome.endIndex))
+// welcome now equals "hello there"
+
+let range = welcome.index(welcome.endIndex, offsetBy: -6)..<welcome.endIndex
+welcome.removeSubrange(range)
+// welcome now equals "hello"
+
+
+
 
---
+++ swiftTutorialForBeginer/StringAndCharacters/SubString.playground/Contents.swift
@@ -0,0 +1,7 @@
+let greeting = "Hello, world!"
+let index = greeting.firstIndex(of: ",") ?? greeting.endIndex
+let beginning = greeting[..<index]
+// beginning is "Hello"
+
+// Convert the result to a String for long-term storage.
+let newString = String(beginning)
