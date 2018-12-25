0937356cf8f731f6982dd3208ec0d28eceb978f3
川内翔一朗 <>
2018-12-26 02:54:57 +0900

    create functions

---
+++ swiftTutorialForBeginer/Functions/FunctionParametersAndReturnValues.playground/Contents.swift
@@ -0,0 +1,103 @@
+func greet(person: String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
+}
+
+
+
+
+
+func greetAgain(person: String) -> String {
+    return "Hello again, " + person + "!"
+}
+
+//Functions Without Parameters
+func sayHelloWorld() -> String {
+    return "hello, world"
+}
+print(sayHelloWorld())
+// Prints "hello, world"
+
+
+func sample() -> String {
+    return "sample"
+}
+
+
+print(sample())
+
+//Functions With Multiple Parameters
+
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+
+print(greet(person: "Tim", alreadyGreeted: true))
+// Prints "Hello again, Tim!"
+
+
+//Functions Without Return Values
+func greetTeacher(person: String) {
+    print("Hello, \(person)!")
+}
+greetTeacher(person: "Dave")
+// Prints "Hello, Dave!"
+
+
+
+func printAndCount(string: String) -> Int {
+    print(string)
+    return string.count
+}
+func printWithoutCounting(string: String) {
+    let _ = printAndCount(string: string)
+}
+printAndCount(string: "hello, world")
+// prints "hello, world" and returns a value of 12
+printWithoutCounting(string: "hello, world")
+// prints "hello, world" but does not return a value
+
+//Functions with Multiple Return Values
+
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+print("min is \(bounds.min) and max is \(bounds.max)")
+// Prints "min is -6 and max is 109"
+
+
+//Optional Tuple Return Types
+
+func minMaxOptional(array: [Int]) -> (min: Int, max: Int)? {
+    if array.isEmpty { return nil }
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+if let bounds = minMaxOptional(array: [8, -6, 2, 109, 3, 71]) {
+    print("min is \(bounds.min) and max is \(bounds.max)")
+}
+// Prints "min is -6 and max is 109"
