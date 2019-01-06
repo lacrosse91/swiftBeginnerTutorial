5cbbcbef95b0efa3527fd3c70cb8e34a27bc4b81
川内翔一朗 <>
2019-01-06 23:46:21 +0900

    doing protocol

---
+++ swiftTutorialForBeginer/Protocols/Protocol.playground/Contents.swift
@@ -0,0 +1,170 @@
+//Protocol Syntax
+
+//protocol SomeProtocol {
+//    // protocol definition goes here
+//}
+//
+//struct SomeStructure: FirstProtocol, AnotherProtocol {
+//    // structure definition goes here
+//}
+//
+//class SomeClass: SomeSuperclass, FirstProtocol, AnotherProtocol {
+//    // class definition goes here
+//}
+
+//Property Requirements
+
+protocol SomeProtocol {
+    var mustBeSettable: Int { get set }
+    var doesNotNeedToBeSettable: Int { get }
+}
+
+protocol AnotherProtocol {
+    static var someTypeProperty: Int { get set }
+}
+
+protocol FullyNamed {
+    var fullName: String { get }
+}
+
+struct Person: FullyNamed {
+    var fullName: String
+}
+let john = Person(fullName: "John Appleseed")
+// john.fullName is "John Appleseed"
+
+
+class Starship: FullyNamed {
+    var prefix: String?
+    var name: String
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    var fullName: String {
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+// ncc1701.fullName is "USS Enterprise"
+
+//Method Requirements
+//protocol SomeProtocol {
+//    static func someTypeMethod()
+//}
+//
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+// Prints "Here's a random number: 0.3746499199817101"
+print("And another one: \(generator.random())")
+// Prints "And another one: 0.729023776863283"
+
+
+//Mutating Method Requirements
+
+protocol Togglable {
+    mutating func toggle()
+}
+
+enum OnOffSwitch: Togglable {
+    case off, on
+    mutating func toggle() {
+        switch self {
+        case .off:
+            self = .on
+        case .on:
+            self = .off
+        }
+    }
+}
+var lightSwitch = OnOffSwitch.off
+lightSwitch.toggle()
+// lightSwitch is now equal to .on
+
+//Initializer Requirements
+
+protocol SomeProtocol1 {
+    init(someParameter: Int)
+}
+
+class SomeClass: SomeProtocol1 {
+    required init(someParameter: Int) {
+        // initializer implementation goes here
+    }
+}
+
+/*
+ protocol SomeProtocol {
+ init()
+ }
+
+ class SomeSuperClass {
+ init() {
+ // initializer implementation goes here
+ }
+ }
+
+ class SomeSubClass: SomeSuperClass, SomeProtocol {
+ // "required" from SomeProtocol conformance; "override" from SomeSuperClass
+ required override init() {
+ // initializer implementation goes here
+ }
+ }
+*/
+
+//Protocols as Types
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
+// Random dice roll is 3
+// Random dice roll is 5
+// Random dice roll is 4
+// Random dice roll is 5
+// Random dice roll is 4
+
+//Delegation
+/*
+ Delegation is a design pattern that enables a class or structure to hand off (or delegate) some of its responsibilities to an instance of another type. This design pattern is implemented by defining a protocol that encapsulates the delegated responsibilities, such that a conforming type (known as a delegate) is guaranteed to provide the functionality that has been delegated. Delegation can be used to respond to a particular action, or to retrieve data from an external source without needing to know the underlying type of that source.
+ */
+
+protocol DiceGame {
+    var dice: Dice { get }
+    func play()
+}
+
+protocol DiceGameDelegate: AnyObject {
+    func gameDidStart(_ game: DiceGame)
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
+    func gameDidEnd(_ game: DiceGame)
+}
+
+
