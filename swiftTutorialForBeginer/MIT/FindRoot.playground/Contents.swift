c80d8d6d6b1ce9be6638bed6c02bdf635d4c2aab
川内翔一朗 <>
2019-03-06 19:08:32 +0900

    二分法を書いた

--- swiftTutorialForBeginer/MIT/FindRoot.playground/Contents.swift
+++ swiftTutorialForBeginer/MIT/FindRoot.playground/Contents.swift
@@ -1,1 +1,24 @@
+import Foundation
+
+let x = 25.0
+let epsilon = 0.01
+var numGusses = 0
+var low = 0.0
+var high = max(1.0, x)
+var ans = (low + high)/2.0
+while fabs(ans*ans - x) >= epsilon {
+    numGusses += 1
+    if (ans*ans < x){
+        low = ans
+    } else {
+        high = ans
+    }
+    ans = (high + low)/2.0
+}
+
+print(numGusses)
+print(ans)
+
+let income = [2,1,4,6]
+income.map { $0 }
 
--- swiftTutorialForBeginer/MIT/NewtonMethod.playground/Contents.swift
+++ swiftTutorialForBeginer/MIT/NewtonMethod.playground/Contents.swift
@@ -6,7 +6,7 @@ let epsilon: Double = 0.001
 let k: Double = 24.0
 
 var guess: Double = k/2
-while sqrt(guess*guess - k) >= epsilon {
+while fabs(guess*guess - k) >= epsilon {
 
     guess = guess - (((guess*guess) - k)/(2*guess))
 }
