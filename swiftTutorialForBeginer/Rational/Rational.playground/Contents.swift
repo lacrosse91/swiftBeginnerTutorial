7069070dc2fcb9005abfa83cafacfa1b2ceca8fe
川内翔一朗 <>
2019-03-06 19:18:37 +0900

    ゼロが分母ならnil

--- swiftTutorialForBeginer/Rational/Rational.playground/Contents.swift
+++ swiftTutorialForBeginer/Rational/Rational.playground/Contents.swift
@@ -5,18 +5,27 @@ var str = "Hello, playground"
 class Rational {
     let numerator: Int
     let denominator: Int
-    init(_ num: Int, _ denom: Int) {
-        
+    init?(_ num: Int, _ denom: Int) {
+
+        guard denom != 0 else { return nil }
+
         self.numerator = num
         self.denominator = denom
     }
     func toString() -> String {
         return "\(numerator)/\(denominator)"
     }
+
+    private func gcd(a: Int, b: Int) -> Int {
+        return 0
+    }
 }
 
 var half = Rational(1, 2)
 
-half.denominator
+half!.denominator
 
-half.toString()
+half!.toString()
+
+var zero = Rational(3,0)
+
